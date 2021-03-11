a4bf432e2f4009806a973994d140f0722ac3a5d1
1n9yun <jig7357@gmail.com>
2021-03-11 13:40:19 +0900

    fix docker-install.sh

--- jenkins/docker-install.sh
+++ jenkins/docker-install.sh
@@ -3,7 +3,7 @@ apt-get update && sudo apt-get install \
     ca-certificates \
     curl \
     software-properties-common && \
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && \
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
